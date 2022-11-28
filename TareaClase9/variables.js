c44210d924367c8d2a0c20221d3ad0c3d23f898e
Javier <holy_gud@hotmail.com>
2022-11-28 19:31:46 -0300

    Tareas

---
+++ TareaClase9/variables.js
@@ -0,0 +1,9 @@
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente_antiguedad = pais + " " + continente + " " + antiguedad;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente_antiguedad);
